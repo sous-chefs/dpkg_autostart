59d39f4621471d52682deee6a9bdf6a955035a01
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-23 09:20:20 -0800

    Update version for development 0.1.11

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name 'dpkg_autostart'
-version '0.1.10'
+version '0.1.11'
 maintainer 'Chris Roberts'
 maintainer_email 'chrisroberts.code@gmail.com'
 description 'Control service actions initialized from dpkg'
