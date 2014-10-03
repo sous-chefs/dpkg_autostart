63edcd32c206dbefca1106fa1b1950a17bc4b805
Chris Roberts <chrisroberts.code@gmail.com>
2014-10-03 15:52:03 -0700

    Include conditional execution on debian families only

--- libraries/provider.rb
+++ libraries/provider.rb
@@ -13,13 +13,14 @@ class Chef
       def action_run
         Chef::Log.debug 'DpkgAutostart: Compat method. Action does nothing.'
       end
-      
+
       def action_create
         template = Chef::Resource::Template.new('dpkg_autostart_file', run_context)
         template.cookbook 'dpkg_autostart'
         template.source 'policy-rc.d.erb'
         template.path '/usr/sbin/policy-rc.d'
         template.mode 0755
+        template.only_if{ node.platform_family?('debian') }
         template.run_action(:create)
         # Force this to the front of the line
       end
